970c9383f59dd6a4ad8c18ba9ed890641e85d34e
Temple <el.temple@gmail.com>
2020-05-25 22:13:40 +0200

    Added html-webpack-plugin, MCEP to webpack config Also devServer has been updated for LiveReload

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,54 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = () => ({ 
 
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, "./dist"),
     index: "index.html",
-    hot: true,
+    hot: false,
+    liveReload: true,
     port: 9000,
-    publicPath: "/js/"
+    publicPath: "/",
+    watchContentBase: true,
+    writeToDisk: true
   },
-  entry: './src/modules/postpone.js',
+  devTool: "source-map",
+  entry: {
+    index:['./src/main.js','./src/assets/css/styles.css']
+  },
+  mode: 'development',
+  module:{
+    rules:[     
+      {
+        test: /\.s?[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader,"css-loader","sass-loader"],
+      },
+  
+    ]
+  },
   output:
   { 
     filename: 'postpone.js',
     path: path.resolve(__dirname,"dist/js"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname,"src/templates/app.html"),
+      filename: path.resolve(__dirname,"dist/index.html"),
+      inject: "body",      
+      minify: false
+    }),
+    new MiniCssExtractPlugin({
+      publicPath: "/assets/css",
+      filename: "../assets/css/styles.css",
+      hmr: true,
+      // if hmr does not work, this is a forceful method.
+      reloadAll: true,
+    }), 
+
+  ],
+
   target: "web"  
 })
