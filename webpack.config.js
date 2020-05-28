79d54a35529fda3e252a08e75634d92865a705f3
Temple <el.temple@gmail.com>
2020-05-28 13:01:56 +0200

    Improved output configuration for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -30,8 +30,8 @@ module.exports = () => ({
   },
   output:
   { 
-    filename: 'postpone.js',
-    path: path.resolve(__dirname,"dist/js"),
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname,"dist"),
   },
   plugins: [
     new HtmlWebpackPlugin({
